b851ec37201d4ff83569fb21cdb760315b2b6401
Lucas Teles <106928828+LucasNerdGeekGames@users.noreply.github.com>
2024-04-03 04:21:32 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -88,15 +88,15 @@ window.addEventListener('load', function(){
                        
             
             // this.frameY = 20; jump
-            if (this.game.keys.includes('ArrowUp')) {
-                this.frameX = 3.7;
-                this.y = 150;
-                this.speedY = this.maxSpeed;
-            }
-            if (this.game.keys.includes('ArrowDown')) {
-                this.frameX = 3.7;
-                this.y = 355;
-            }
+            //if (this.game.keys.includes('ArrowUp')) {
+            //    this.frameX = 3.7;
+            //    this.y = 150;
+            //    this.speedY = this.maxSpeed;
+            //}
+            //if (this.game.keys.includes('ArrowDown')) {
+            //    this.frameX = 3.7;
+            //    this.y = 355;
+            //}
             //else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
             //else this.speedY = 0;
             //this.y += this.speedY;
